eb2c684485828914e3eee62be5f9101ff7cc2616
MiranCZ <miran08.mail@gmail.com>
2024-06-10 18:01:17 +0200

    made baritone break infested blocks

---
+++ src/main/java/adris/altoclef/mixins/MovementHelperMixin.java
@@ -0,0 +1,24 @@
+package adris.altoclef.mixins;
+
+import baritone.pathing.movement.MovementHelper;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.InfestedBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(MovementHelper.class)
+public interface MovementHelperMixin {
+
+    @Redirect(method = "avoidBreaking", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;", ordinal = 1))
+    private static Block allowInfested(BlockState instance) {
+        // we are able to handle breaking infested blocks...
+        if (instance.getBlock() instanceof InfestedBlock infestedBlock) {
+            return infestedBlock.getRegularBlock();
+        }
+
+        return instance.getBlock();
+    }
+
+}
