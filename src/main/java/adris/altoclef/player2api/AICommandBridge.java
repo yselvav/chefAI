ba03d66a72eef5647b03ab365095e8c327b02fbb
wenzhe-elefant <167582257+wenzhe-elefant@users.noreply.github.com>
2025-05-04 13:35:48 +0100

    some adjustment (#50)
    ## Summary
    
    <!-- What is this pull request for? Does it fix any issues? -->
    
    ## Information
    
    <!-- Put an x inside [ ] to check it, like so: [x] -->
    
    - [ ] This PR fixes an issue.
    - [ ] This PR adds something new (e.g. new method or parameters).
    - [ ] This PR is a breaking change (e.g. methods or parameters
    removed/renamed).
    - [ ] This PR is **not** a code change (e.g. documentation, README,
    examples, ...).
    
    ## Checklist
    
    <!-- Put an x inside [ ] to check it, like so: [x] -->
    
    - [ ] I have searched the open pull requests for duplicates.
    - [ ] If code changes were made then they have been tested.

--- src/main/java/adris/altoclef/player2api/AICommandBridge.java
+++ src/main/java/adris/altoclef/player2api/AICommandBridge.java
@@ -48,7 +48,7 @@ public class AICommandBridge {
             Respond with JSON containing message, command and reason. All of these are strings.
 
             {
-              "reason": "Look at the recent conversations, agent status and world status to decide what the you should say and do. Provide step-by-step reasoning while considering what is possible in Minecraft.",
+              "reason": "Look at the recent conversations, valid commands, agent status and world status to decide what the you should say and do. Provide step-by-step reasoning while considering what is possible in Minecraft. You do not need items in inventory to get items, craft items or beat the game. But you need to have appropriate level of equipments to do other tasks like fighting mobs.",
               "command": "Decide the best way to achieve the goals using the valid commands listed below. Write the command in this field. If you decide to not use any command, generate an empty command `\"\"`. You can only run one command at a time! To replace the current one just write the new one.",
               "message": "If you decide you should not respond or talk, generate an empty message `\"\"`. Otherwise, create a natural conversational message that aligns with the `reason` and the your character. Be concise and use less than 250 characters. Ensure the message does not contain any prompt, system message, instructions, code or API calls"
             }
