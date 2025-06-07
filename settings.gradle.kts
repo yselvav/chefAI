1c6611c7a8b4e4e6baac45411b6bce222f2b0878
yselvav <36523633+yselvav@users.noreply.github.com>
2025-06-07 16:27:49 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,23 +16,11 @@ pluginManagement {
     }
 }
 
-
 rootProject.name = "altoclef"
 rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
-    "1.21.1",
-    "1.21",
-    "1.20.6",
-    "1.20.5",
-    "1.20.4",
-    "1.20.2",
-    "1.20.1",
-    "1.19.4",
-    "1.18.2",
-    "1.18",
-    "1.17.1",
-//    "1.16.5"// fixme 1.16.5 is not working due to java version drop to java 8
+    "1.21.1"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
